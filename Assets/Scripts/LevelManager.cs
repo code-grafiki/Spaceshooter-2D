cec6caf7199c6cda5bf673677b3ee59c1f1f06ca
code-grafiki <mkishore3002@gmail.com>
2024-07-05 20:16:05 +0530

    add level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Level 0");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGameOVer()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
